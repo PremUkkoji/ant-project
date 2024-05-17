86cecff8a87bedbf3dd7a95a29f195b8cb56a493
Prem Ukkoji <premukkoji@gmail.com>
2024-05-17 20:49:07 +0530

    learning ant

---
+++ src/pu/HelloWorld.java
@@ -0,0 +1,11 @@
+package pu;
+
+import org.apache.log4j.Logger;
+
+public class HelloWorld {
+  private static Logger logger = Logger.getLogger(HelloWorld.class);
+
+  public static void main(String[] args) {
+    logger.info("Hello World");
+  }
+}
